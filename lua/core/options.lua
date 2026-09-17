2981cbe13f809c13c86defc21127bef6f4b7fbaf
ahoefn <hoefnagelsarno@gmail.com>
2026-09-17 21:05:55 +0200

    added border to boxes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,7 +3,16 @@
 -- formatting
 vim.o.autoindent = true
 vim.o.expandtab = true
+vim.o.winborder  = "rounded"
 
+
+	-- "bold": Bold line box.
+	-- "double": Double-line box.
+	-- "none": No border.
+	-- "rounded": Like "single", but with rounded corners ("╭" etc.).
+	-- "shadow": Drop shadow effect, by blending with the background.
+	-- "single": Single-line box.
+	-- "solid": Adds padding by a single whitespace cell.
 -- visual
 vim.o.cmdheight = 2
 vim.o.winborder = 'rounded'
